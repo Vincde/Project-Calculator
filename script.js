e302ab22df4a8e30c00a2f5dd24fc30530f6cdce
Vincde <145802178+Vincde@users.noreply.github.com>
2024-01-16 15:11:34 +0100

    Insert add,subtract,multiply,divide functions

---
+++ script.js
@@ -0,0 +1,31 @@
+function add(a,b){
+    return a + b;
+}
+
+function subtract(a,b){
+    return a - b;
+}
+
+function multiply(a,b){
+    return a * b;
+}
+
+function divide(a,b){
+    return a / b;
+}
+
+/* function calculator(str){
+    
+    let arr;
+    arr = str.split(' ');
+
+    let a = +arr[0];
+    let b = +arr[2];
+
+    let op = arr[1];
+
+    if(op === '+') return add(a,b);
+    else if(op === '-') return subtract(a,b);
+    else if(op === '*') return multiply(a,b);
+    else if(op === '/') return multiply(a,b);
+} */
